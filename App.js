f34a7e06a949655e8aa2f72d92d7fb582be14117
Shivam Bansal <shivam2032.cse18@chitkara.edu.in>
2021-06-05 17:22:29 +0530

    adding FlatList

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput,ScrollView } from "react-native";
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList
+} from 'react-native';
 
 export default function App() {
-  const [enetredGoal, setEnteredGoal] = useState("");
+  const [enteredGoal, setEnteredGoal] = useState('');
   const [courseGoals, setCourseGoals] = useState([]);
 
-  const goalInputHandler = (enteredText) => {
+  const goalInputHandler = enteredText => {
     setEnteredGoal(enteredText);
-    
   };
 
   const addGoalHandler = () => {
-    setCourseGoals((currentGoals) => [...currentGoals, enetredGoal]);
-    setEnteredGoal("");
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: enteredGoal }
+    ]);
   };
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
@@ -21,37 +31,43 @@ export default function App() {
           placeholder="Course Goals"
           style={styles.input}
           onChangeText={goalInputHandler}
-          value={enetredGoal}
+          value={enteredGoal}
         />
         <Button title="ADDD" onPress={addGoalHandler} />
       </View>
-      <ScrollView>
-        {courseGoals.map((goal)=><View  key={goal} style={styles.items}><Text>{goal}</Text></View>)}
-      </ScrollView>
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 50,
+    padding: 50
   },
   inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
   input: {
-    width: "80%",
-    borderColor: "black",
+    width: '80%',
+    borderColor: 'black',
     borderWidth: 1,
-    padding: 6,
+    padding: 10
   },
-  items:{
-    padding:10,
-    marginVertical:10,
-    borderColor: "black",
-    backgroundColor:"#ccc",
-    borderWidth:1
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
   }
 });
