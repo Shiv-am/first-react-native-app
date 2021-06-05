bf74a9830e70c49d767c3c35fc0cbcc7ca010f95
Shivam Bansal <shivam2032.cse18@chitkara.edu.in>
2021-06-05 16:55:17 +0530

    updtaed app for adding items

--- App.js
+++ App.js
@@ -1,23 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button} from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-  const [outputText,setoutputText] = useState('Open up App.js to start working on your app!')
+  const [enetredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+    
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals((currentGoals) => [...currentGoals, enetredGoal]);
+    setEnteredGoal("");
+  };
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="change text" onPress={()=> setoutputText('Text is changed..')}/>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goals"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enetredGoal}
+        />
+        <Button title="ADDD" onPress={addGoalHandler} />
+      </View>
+      <View>
+        {courseGoals.map((goal)=><Text key={goal}>{goal}</Text>)}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 6,
   },
 });
